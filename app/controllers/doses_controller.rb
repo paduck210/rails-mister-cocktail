be91c25810834cafbf024f61c3a865620c4641b2
Jung Yu <hjyu1408@gmail.com>
2019-11-14 17:38:17 -0500

    backend

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,35 @@
+class DosesController < ApplicationController
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @ingredients = Ingredient.all
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    # raise
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+end
+
+# ingredient_id
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Starts"
+
+Ingredient.destroy_all
+Cocktail.destroy_all
+Dose.destroy_all
+
+
+in_1 = Ingredient.create(name: "lemon")
+in_2 = Ingredient.create(name: "ice")
+in_3 = Ingredient.create(name: "mint leaves")
+
+
+co_1 = Cocktail.create(name: "Lemon Tree")
+co_2 = Cocktail.create(name: "Sex on the beach")
+co_3 = Cocktail.create(name: "Blue Bottle")
+
+
+do_1 = Dose.new(description: "5 spoons, 3 sugar")
+do_2 = Dose.new(description: "10 shots, 2 pouder")
+do_3 = Dose.new(description: "3 spoons, pork, noodle")
+
+
+do_1.ingredient = in_1
+do_2.ingredient = in_2
+do_3.ingredient = in_3
+
+
+do_1.cocktail = co_1
+do_2.cocktail = co_2
+do_3.cocktail = co_3
+
+
+do_1.save
+do_2.save
+do_3.save
+
+puts "Done"
